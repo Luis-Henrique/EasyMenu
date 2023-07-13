17f5e3069d08d22a7492a08174c242da6c51c88b
Luis Henrique Gomes Cardoso <luis.cardoso@dotzmkt.com.br>
2023-07-12 22:39:19 -0300

    chore: Startup and Program configuration

--- EasyMenu.Api.Admin/Program.cs
+++ EasyMenu.Api.Admin/Program.cs
@@ -1,25 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace EasyMenu.API.Admin
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+        }
+    }
 }
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
 
-app.MapControllers();
-
-app.Run();
---
+++ EasyMenu.Api.Admin/Startup.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+
+namespace EasyMenu.API.Admin
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+        readonly string CorsPolicy = "_corsPolicy";
+        public void BeforeConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            Configuration = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.json")
+               .Build();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: CorsPolicy,
+                builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+            BeforeConfigureServices(services);
+
+            services.AddSwaggerGen();
+   
+
+            services.AddMvc(options =>
+            {
+                options.EnableEndpointRouting = false;
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            var supportedCultures = new[] { new CultureInfo("en-US") };
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseCors(CorsPolicy);
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseMvc();
+        }
+    }
+}
