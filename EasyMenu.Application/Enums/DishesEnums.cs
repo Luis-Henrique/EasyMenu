2e93d33ee7e8aa9bc64667a5ad158038bdfe952f
Luis Henrique Gomes Cardoso <luis.cardoso@dotzmkt.com.br>
2023-07-15 00:26:24 -0300

    feat(enums): API Enums
    Adding portion enum of dishes table

---
+++ EasyMenu.Application/Enums/DishesEnums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMenu.Application.Enums
+{
+    public enum Portion
+    {
+        Pequeno = 1,
+        Medio = 2,
+        Grande = 3,
+    }
+}
