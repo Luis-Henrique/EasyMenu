2971b0808d4881326d16cf771b860718e1c1d07e
Luis Henrique Gomes Cardoso <luis.cardoso@dotzmkt.com.br>
2023-07-15 00:25:00 -0300

    feat(helpers): Adding Utils, ResultData and ErrorResponse

---
+++ EasyMenu.Application/Helpers/ErrorResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMenu.Application.Helpers
+{
+    public class ErrorResponse
+    {
+        public ErrorResponse(string error)
+        {
+            this.Errors = new List<string>();
+            this.Errors.Add(error);
+        }
+
+        public ErrorResponse(List<string> errorList)
+        {
+            this.Errors = new List<string>();
+            this.Errors = errorList;
+        }
+
+        public ErrorResponse()
+        {
+            this.Errors = new List<string>();
+        }
+        public List<string> Errors { get; set; }
+    }
+}
---
+++ EasyMenu.Application/Helpers/ResultData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMenu.Application.Helpers
+{
+    public class ResultData
+    {
+        public ResultData(object _data, bool _success)
+        {
+            this.Success = _success;
+            this.Data = _data;
+        }
+        public bool Success { get; set; }
+        public object Data { get; set; }
+    }
+}
