74885df02fd3edf4e6563840c46cd718f12440f7
Luis Henrique Gomes Cardoso <luis.cardoso@dotzmkt.com.br>
2023-07-15 22:52:04 -0300

    feat(users): Adding user authentication
    user route and basic authentication helper

---
+++ EasyMenu.Application/Data/SqlServer/Repositories/UserRepository.cs
@@ -0,0 +1,71 @@
+﻿using Dapper;
+using EasyMenu.Application.Contracts.Response;
+using EasyMenu.Application.Data.MySql.Entities;
+using EasyMenu.Application.Data.SqlServer;
+using EasyMenu.Application.Interfaces;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using static Dapper.SqlMapper;
+
+namespace EasyMenu.Application.Data.MySql.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly SqlServerContext _context;
+
+        public UserRepository(SqlServerContext ctt)
+        {
+            _context = ctt;
+        }
+
+        public async Task<DefaultResponse> CreateUser(UserEntity entity) 
+        {
+            _context.User.Add(entity);
+            var result = await this.SaveAllAsync();
+
+            if (result == true)
+               return new DefaultResponse(entity.Id.ToString(), "Conta criada com sucesso",false);
+
+            return new DefaultResponse("", "Erro ao tentar criar uma conta", true);
+        }
+
+        public async Task<DefaultResponse> UpdateUser(UserEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            var result = await this.SaveAllAsync();
+
+            if (result == true)
+               return new DefaultResponse(entity.Id.ToString(), "Senha de usuário alterada com sucesso", false);
+
+            return new DefaultResponse(entity.Id.ToString(), "Erro ao tentar alterar senha de um usuário", true);
+        }
+
+        public async Task<UserEntity> GetUserByEmail(string email)
+        {
+            return await _context.User.Where(x => x.Email == email).FirstOrDefaultAsync();
+        }
+
+        public async Task<UserEntity> GetUserByUserNameAndEmail(string username, string email)
+        {
+            return await _context.User.Where(x => x.Email == email).Where(x => x.UserName == username).FirstOrDefaultAsync();
+        }
+
+        public async Task<UserEntity> GetUserByCredentialsAsync(string email, string password)
+        {
+            return await _context.User.Where(x => x.Email == email).Where(x => x.Password == password).FirstOrDefaultAsync();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ EasyMenu.Application/Services/UserService.cs
@@ -0,0 +1,74 @@
+﻿using EasyMenu.Application.Contracts;
+using EasyMenu.Application.Contracts.Request.Users;
+using EasyMenu.Application.Data.MySql.Entities;
+using EasyMenu.Application.Data.MySql.Repositories;
+using EasyMenu.Application.Helpers;
+using EasyMenu.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EasyMenu.Application.Services
+{
+    public class UserService
+    {
+        private readonly IUserRepository _userRepository;
+        public UserService(IUserRepository userRepository)
+        {
+            this._userRepository = userRepository;
+        }
+        public async Task<ResultData> PostAsync(UserPostRequest request)
+        {
+            var entity = new UserEntity(request);
+            var response = await _userRepository.CreateUser(entity);
+            Console.WriteLine("Sucesso" + DateTime.Now + "\r\n");
+            return Utils.SuccessData(response);
+        }
+
+        public async Task<ResultData> PutAsync(UserPutRequest request)
+        {
+            var existUser = await _userRepository.GetUserByUserNameAndEmail(request.UserName, request.Email);
+
+            if (existUser != null)
+            {
+                var entity = new UserEntity(request);
+                var result = await _userRepository.UpdateUser(entity);
+
+                if (!result.HasErrors) 
+                   return Utils.SuccessData(result);
+
+                return Utils.ErrorData(result);
+            }
+            throw new NotImplementedException();
+            //return Utils.ErrorData(DocManagerErrors.User_Put_BadRequest_User_Not_Found.Description());
+        }
+
+        public async Task<UserEntity> Authenticate(string user, string password)
+        {
+            var response = await _userRepository.GetUserByCredentialsAsync(user, password);
+            return response;
+        }
+
+
+        public async Task<ResultData> PostLoginAsync(UserPostLoginRequest user)
+        {
+            var openData = user.Email + ":" + user.Password + ":" + Utils.GetDateExpired(10);
+            var dataBytes = Utils.ToBase64Encode(openData);
+            var getuser = await _userRepository.GetUserByCredentialsAsync(user.Email, user.Password);
+
+            if (getuser != null)
+            {
+                var response = new AccountResponse
+                {
+                    Id = getuser.Id.ToString(),
+                    Message = "Token successful",
+                    Token = dataBytes
+                };
+                return Utils.SuccessData(response);
+            }
+
+            return Utils.ErrorData(new AccountResponse { Message = "Token Fail" });
+        }
+
+    }
+}
